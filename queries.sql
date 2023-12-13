ab76c5d27ae7603cb175f9184875c234db41a0bb
olegenok03 <61906662+olegenok03@users.noreply.github.com>
2023-12-13 16:24:47 +0300

    Create queries.sql
    2 new queries have been added

---
+++ queries.sql
@@ -0,0 +1,22 @@
+-- Вывести все подарки (Gift), сделанные пользователю c User.id = 5, по убыванию даты
+
+SELECT g.id, g.title, g.message, p.product_id, p.date
+	FROM Gift AS g
+	INNER JOIN Purchase AS p
+	ON p.id = 5 AND g.purchase_id = 5
+	ORDER BY date DESC;
+
+
+-- Вывести 10 издателей (Publisher), лидирующих по объему продаж их игр
+
+WITH publisher_sales AS (
+	SELECT publisher_id, SUM(purchasers_count) AS sales
+	FROM Product AS prod
+	GROUP BY publisher_id
+)
+SELECT pub.id, pub.name, pub.description, s.sales
+	FROM publisher_sales AS s
+	INNER JOIN Publisher AS pub
+	ON s.publisher_id = pub.id
+	ORDER BY sales DESC
+	LIMIT 10;
