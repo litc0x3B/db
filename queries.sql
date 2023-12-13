7f333b2743a4d610828efa2a8387e8ff1034ca49
olegenok03 <61906662+olegenok03@users.noreply.github.com>
2023-12-13 16:31:39 +0300

    Update queries.sql
    gifts query has been edited again

--- queries.sql
+++ queries.sql
@@ -1,9 +1,9 @@
--- Вывести все подарки (Gift), сделанные пользователю c User.id = 5, по убыванию даты
+-- Вывести все подарки (Gift), сделанные пользователю c User.id = 2, по убыванию даты
 
 SELECT g.id, g.title, g.message, p.product_id, p.date
 	FROM Gift AS g
 	INNER JOIN Purchase AS p
-	ON p.recipient_id = 5 AND g.purchase_id = p.id
+	ON g.recipient_id = 2 AND g.purchase_id = p.id
 	ORDER BY date DESC;
 
 
